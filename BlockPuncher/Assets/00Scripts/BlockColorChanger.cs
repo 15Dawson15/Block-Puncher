3b2de5e1726050b9737a596f15be31534bdb3d6b
15Dawson15 <39501017+15Dawson15@users.noreply.github.com>
2018-07-11 15:15:45 -0700

    BlockColorChanger changes

--- BlockPuncher/Assets/00Scripts/BlockColorChanger.cs
+++ BlockPuncher/Assets/00Scripts/BlockColorChanger.cs
@@ -14,53 +14,69 @@ public class BlockColorChanger : MonoBehaviour {
         public Vector2 TouchpadPosition;
     }
 
-    private Dictionary<uint, ControllerState> controllers;
+    //private Dictionary<uint, ControllerState> controllers;
 
     private void Awake()
     {
-        controllers = new Dictionary<uint, ControllerState>();
+        //controllers = new Dictionary<uint, ControllerState>();
 
-        InteractionManager.InteractionSourceDetected += InteractionManager_InteractionSourceDetected;
+        //InteractionManager.InteractionSourceDetected += InteractionManager_InteractionSourceDetected;
 
-        InteractionManager.InteractionSourceLost += InteractionManager_InteractionSourceLost;
-        InteractionManager.InteractionSourceUpdated += InteractionManager_InteractionSourceUpdated;
+        //InteractionManager.InteractionSourceLost += InteractionManager_InteractionSourceLost;
+        //InteractionManager.InteractionSourceUpdated += InteractionManager_InteractionSourceUpdated;
     }
 
-   
 
-    
-
-    private void InteractionManager_InteractionSourceDetected(InteractionSourceDetectedEventArgs obj)
+    private void Update()
     {
-        Debug.LogFormat("{0} {1} Detected", obj.state.source.handedness, obj.state.source.kind);
+        var interactionSourceStates = InteractionManager.GetCurrentReading();
 
-        if (obj.state.source.kind == InteractionSourceKind.Controller && !controllers.ContainsKey(obj.state.source.id))
+        foreach(var interactionSourceState in interactionSourceStates)
         {
-            controllers.Add(obj.state.source.id, new ControllerState { Handedness = obj.state.source.handedness });
+            if (interactionSourceState.touchpadTouched && interactionSourceState.source.handedness == InteractionSourceHandedness.Right)
+            {
+                Debug.Log("This is the right hand: " + interactionSourceState.touchpadPosition);
+            }
+            if (interactionSourceState.touchpadTouched && interactionSourceState.source.handedness == InteractionSourceHandedness.Left)
+            {
+                Debug.Log("This is the left hand: " + interactionSourceState.touchpadPosition);
+            }
         }
     }
 
-    private void InteractionManager_InteractionSourceLost(InteractionSourceLostEventArgs obj)
-    {
-        Debug.LogFormat("{0} {1} Lost", obj.state.source.handedness, obj.state.source.kind);
 
-        controllers.Remove(obj.state.source.id);
-    }
+    //private void InteractionManager_InteractionSourceDetected(InteractionSourceDetectedEventArgs obj)
+    //{
+    //    Debug.LogFormat("{0} {1} Detected", obj.state.source.handedness, obj.state.source.kind);
 
-    private void InteractionManager_InteractionSourceUpdated(InteractionSourceUpdatedEventArgs obj)
-    {
-        ControllerState controllerState;
+    //    if (obj.state.source.kind == InteractionSourceKind.Controller && !controllers.ContainsKey(obj.state.source.id))
+    //    {
+    //        controllers.Add(obj.state.source.id, new ControllerState { Handedness = obj.state.source.handedness });
 
-        if (controllers.TryGetValue(obj.state.source.id, out controllerState))
-        {
-            controllerState.TouchpadPressed = obj.state.touchpadPressed;
-            controllerState.TouchpadTouched = obj.state.touchpadTouched;
-            controllerState.TouchpadPosition = obj.state.touchpadPosition;
-            Debug.Log("controllerState.TouchPadPressed: " + controllerState.TouchpadPressed);
-            Debug.Log("controllerState.TouchpadTouched: " + controllerState.TouchpadTouched);
-            Debug.Log("controllerState.TouchpadPosition: " + controllerState.TouchpadPosition);
-        }
-    }
+    //    }
+    //}
+
+    //private void InteractionManager_InteractionSourceLost(InteractionSourceLostEventArgs obj)
+    //{
+    //    Debug.LogFormat("{0} {1} Lost", obj.state.source.handedness, obj.state.source.kind);
+
+    //    controllers.Remove(obj.state.source.id);
+    //}
+
+    //private void InteractionManager_InteractionSourceUpdated(InteractionSourceUpdatedEventArgs obj)
+    //{
+    //    ControllerState controllerState;
+
+    //    if (controllers.TryGetValue(obj.state.source.id, out controllerState))
+    //    {
+    //        controllerState.TouchpadPressed = obj.state.touchpadPressed;
+    //        controllerState.TouchpadTouched = obj.state.touchpadTouched;
+    //        controllerState.TouchpadPosition = obj.state.touchpadPosition;
+    //        Debug.Log("controllerState.TouchPadPressed: " + controllerState.TouchpadPressed);
+    //        Debug.Log("controllerState.TouchpadTouched: " + controllerState.TouchpadTouched);
+    //        Debug.Log("controllerState.TouchpadPosition: " + controllerState.TouchpadPosition);
+    //    }
+    //}
 
     //private string GetControllerInfo()
     //{
