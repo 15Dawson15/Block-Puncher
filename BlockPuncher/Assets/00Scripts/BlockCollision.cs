c7cc666a80907cbb65451d525701b10862461732
15Dawson15 <39501017+15Dawson15@users.noreply.github.com>
2018-07-12 09:49:11 -0700

    Block Movement and Collision Added

---
+++ BlockPuncher/Assets/00Scripts/BlockCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCollision : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        BlockColorChanger controllerScript = collision.gameObject.GetComponent<BlockColorChanger>();
+        if(controllerScript != null)
+        {
+            Destroy(this);
+        }
+    }
+}
---
+++ BlockPuncher/Assets/00Scripts/BlockMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody blockBody;
+
+	// Use this for initialization
+	void Start () {
+        blockBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        blockBody.velocity = new Vector3(0, 0, speed);
+	}
+}
